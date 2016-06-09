694e17c03128386b6c3485590f5e21851d4977c6
Zachary Stegall <zachstegall@gmail.com>
2016-06-09 09:31:47 -0700

    xcode proj

---
+++ ZipperSlug/ZipperSlug.h
@@ -0,0 +1,13 @@
+//
+//  ZipperSlug.h
+//  ZipperSlug
+//
+//  Created by Zachary Stegall on 6/9/16.
+//  Copyright © 2016 Zachary Stegall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZipperSlug : NSObject
+
+@end
---
+++ ZipperSlug/ZipperSlug.m
@@ -0,0 +1,13 @@
+//
+//  ZipperSlug.m
+//  ZipperSlug
+//
+//  Created by Zachary Stegall on 6/9/16.
+//  Copyright © 2016 Zachary Stegall. All rights reserved.
+//
+
+#import "ZipperSlug.h"
+
+@implementation ZipperSlug
+
+@end
